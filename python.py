e5de55e607f711efca951c30c6cf9fd0f2aa4df1
NataliyaDi <41993413+NataliyaDi@users.noreply.github.com>
2021-04-14 19:01:08 +0300

    Create python.py

---
+++ python.py
@@ -0,0 +1,66 @@
+# Python data types
+
+
+l1 = [1, 2, 3, 4, 5]
+l1[1:3][0]=1000
+print(l1)  #???
+
+l2 = [1, [0, 2], 3, 4, 5]
+l2[1:3][0][0]=1000
+print(l2)  #???
+
+
+# Visibility
+
+
+def a():
+    V = 2
+    print(V)
+def b(V=3):
+    print(V)
+def c():
+    print(V)
+
+V = 1
+
+a()  # ???
+b()  # ???
+c()  # ???
+
+
+
+def func(L=[]):
+    L.append(1)
+    return L
+
+print(func())    # ???
+print(func())    # ???
+print(func([]))  # ???
+print(func())    # ???
+
+
+
+# Class and inheritance
+
+
+class A:
+    def m(self):
+        print('A')
+
+class B:
+    def m(self):
+        print('B')
+
+class C(B, A):
+    pass
+
+class D(A, B):
+    pass
+
+class E(A, B):
+    def m(self):
+        print('E')
+
+C().m()  # ???
+D().m()  # ???
+E().m()  # ???
